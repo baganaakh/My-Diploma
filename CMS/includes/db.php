dc72e22cc7510cd62ed119c0cdcb5e811d21350c
baganaakh <baganaakh@gmail.com>
2019-03-11 10:27:16 +0800

    delete

--- CMS/includes/db.php
+++ CMS/includes/db.php
@@ -7,8 +7,6 @@ foreach ($db as $key => $value) {
     define(strtoupper($key), $value);
 }
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-// if ($connection) {
-//     echo " WEE connected yeah ";
-// }
+
 
 ?>
