84798ee354b77ca1274d563e2a799ceaf3884a56
baganaakh <baganaakh@gmail.com>
2019-03-21 10:00:32 +0800

    move to test

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -27,9 +27,22 @@ if (isset($_POST['create_post'])) {
         <input type="text" class="form-control" name="title">
     </div>
     <div class="form-group">
-        <label for="post_category">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <select name="post_category" id="">
+            <?php 
+            $query = "SELECT * FROM categories";
+            $select_categories = mysqli_query($connection, $query);
+            comfirm($select_categories);
+            while ($row = mysqli_fetch_assoc($select_categories)) {
+                $cat_id = $row['cat_id'];
+                $cat_title =$row['cat_title'];
+                echo "<option value=''>{$cat_title}</option>";
+            }
+
+            ?>
+
+        </select>
     </div>
+
     <div class="form-group">
         <label for="title">Post Author</label>
         <input type="text" class="form-control" name="author">
